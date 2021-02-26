7f8e60b16408c127ee0edaff9ef26c9a43baece2
InstaHeat <urk91@mail.ru>
2021-02-26 21:31:26 +0300

    init: reverse int task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const str = n.toString()
+                 .split('')
+                 .reverse()
+                 .join('');
+    const res = Number.parseInt(str,10);
+    return res;
 }
